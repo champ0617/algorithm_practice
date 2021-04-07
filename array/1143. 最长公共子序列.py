eed807e8cdc18348c1e824f5de730d8fb097a312
chenbin <13260609707@163.com>
2021-04-07 17:04:22 +0800

    XX

---
+++ array/1143. 最长公共子序列.py
@@ -0,0 +1,9 @@
+"""
+1 <= text1.length, text2.length <= 1000
+text1 和 text2 仅由小写英文字符组成。
+"""
+
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        pass
