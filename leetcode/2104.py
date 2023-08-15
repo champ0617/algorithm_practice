2fbf6200b2fbf0c1a447276ca8d101a989d3bb6a
chenbin <chenbin4@nsfocus.com>
2023-08-15 17:45:13 +0800

    XX

---
+++ leetcode/2104.py
@@ -0,0 +1,9 @@
+"""
+子数组范围和
+"""
+from typing import List
+
+
+class Solution(object):
+    def subArrayRanges(self, num: List[int]) -> int:
+        pass
---
+++ leetcode/lc_564.py
@@ -0,0 +1,8 @@
+"""
+寻找最近的回文数
+"""
+
+
+class Solution(object):
+    def nearestPalindromic(self, n: str) -> str:
+        pass
